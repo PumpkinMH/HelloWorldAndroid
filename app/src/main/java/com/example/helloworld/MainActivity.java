d118e9878119059a00886e6e746b925d31e2f41f
Mabel <mahurd@email.sc.edu>
2025-05-15 12:38:48 -0400

    Added button to demonstrate notifications along with toast to request notifications

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.helloworld;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int notifIdCount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +35,42 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // Toast stuff
         Button toastButton = findViewById(R.id.toastButton);
         toastButton.setOnClickListener(v -> {
             Toast.makeText(this, R.string.toast_content, Toast.LENGTH_SHORT).show();
+        });
+
+        // Notification stuff
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        final String CHANNEL_ID = "test_notifications";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_onesignal_default)
+                .setContentTitle("Test Notification")
+                .setContentText("Hello Notification")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Button notificationButton = findViewById(R.id.notificationButton);
+        notificationButton.setOnClickListener(v -> {
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+                builder.setContentTitle("Test Notification " + notifIdCount);
+                manager.notify(notifIdCount++, builder.build());
+            } else {
+                Toast.makeText(this, R.string.notification_toast, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
